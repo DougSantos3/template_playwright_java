da23d19a25a46f99b6c7070799c48ef92c115ba3
douglas <douglasmartinssantos7@gmail.com>
2024-10-30 16:25:53 -0300

    new page object

---
+++ src/test/java/com/example/pages/KanbanPage.java
@@ -0,0 +1,31 @@
+package com.example.pages;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class KanbanPage {
+    private Page page;
+    private Locator param1;
+    private Locator param2;
+    private Locator param3;
+
+    // Construtor para inicializar a página e os elementos
+    public KanbanPage(Page page) {
+        this.page = page;
+        this.param1 = page.locator("");
+        this.param2 = page.locator("");
+        this.param3 = page.locator("");
+    }
+
+    // Método para realizar o login
+    public void login(String username, String password) {
+        param1.fill(username);
+        param2.fill(password);
+        param3.click();
+    }
+
+    // Método para verificar se o login falhou
+    public boolean isLoginErrorVisible() {
+        Locator errorMessage = page.locator(".error-message");
+        return errorMessage.isVisible();
+    }
+}
